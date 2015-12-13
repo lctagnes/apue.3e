750380aa34a02eea9d5322312c39de558d7bbfa7
lctagnes <378640378@qq.com>
2015-12-13 17:48:48 +0800

    getuid and getgid

---
+++ Chapter1/1-9.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 1-9.c
+	> Author: CuitingLin
+	> Mail: agnes2729@sina.com 
+	> Created Time: Sun 13 Dec 2015 05:46:14 PM CST
+ ************************************************************************/
+
+#include "apue.h"
+
+int main(void)
+{
+	printf("uid = %d, gid = %d\n", getuid(), getgid());
+	exit(0);
+}
