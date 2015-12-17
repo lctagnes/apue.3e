4e3aa845e92c0562971b9e2a050c167814468432
lctagnes <378640378@qq.com>
2015-12-17 23:25:08 +0800

    setjmp and longjmp

---
+++ Chapter7/7-5.c
@@ -0,0 +1,58 @@
+/*************************************************************************
+	> File Name: 7-5.c
+	> Author: lctagnes
+	> Mail: agnes2729@sina.com 
+	> Created Time: 2015年12月17日 星期四 15时56分27秒
+ ************************************************************************/
+#include "apue.h"
+#include <setjmp.h>
+
+#define TOK_ADD 5
+jmp_buf jmpbuffer;
+
+void do_line(char *);
+void cmd_add(void);
+int get_token(void);
+
+int main(void)
+{
+	char line[MAXLINE];
+
+	if(setjmp(jmpbuffer) != 0)
+		printf("error");
+	while(fgets(line, MAXLINE, stdin) != NULL)
+		do_line(line);
+	exit(0);
+}
+
+void cmd_add(void)
+{
+	int token;
+	token = get_token();
+	if(token < 0)
+		longjmp(jmpbuffer, 1);
+}
+
+char *tok_ptr;
+
+void do_line(char *ptr)
+{
+	int cmd;
+
+	tok_ptr = ptr;
+	while((cmd = get_token()) > 0)
+	{
+		switch(cmd)
+		{
+			case TOK_ADD:
+				cmd_add();
+				break;
+		}
+	}
+}
+
+int get_token(void)
+{
+
+}
+
